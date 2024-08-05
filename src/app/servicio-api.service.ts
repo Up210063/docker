39989fa9a6c966f139ca24adc9a9ad756d4bd8a1
GusGit2003 <gusavegue@gmail.com>
2024-08-04 23:41:52 -0600

    Hola

---
+++ src/app/servicio-api.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'; //Configuración del uso de la librería HttpClient
+import { take } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServicioAPIService {
+
+  constructor(private http: HttpClient) { };
+
+  retornar() {
+    return this.http.get("https://gaia.inegi.org.mx/wscatgeo/mgee/").pipe(take(1));
+    /* El get permite indicar la URL del API que se quiere.
+    El pipe(take(1)) es para que el API sólo sea llamado una vez, el get retorna un observable que
+    cada vez que existan cambios en los datos de esa URL seremos informados, por esto sólo le decimos
+    que sólo queremos los datos una vez. */
+  }
+} //Fin de la Clase
